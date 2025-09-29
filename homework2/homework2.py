a8c5348ca3b2e10c1fe88184110df1634bddb812
nandi-astro <nandini.prasad@berkeley.edu>
2025-09-29 12:45:29 -0700

    done with hw2

---
+++ homework2/homework2.py
@@ -0,0 +1,57 @@
+# File: homework2.py
+
+# Your file path should look like:
+# python_decal_fa25/yourname/homework2/homework2.py
+
+# Questions (Answer these in the homework2.py file as comments):
+
+# 1) What’s the difference between Git, GitHub, and Git Bash?
+#Git operates locally on your computer, and tracks changes in files and coordinates your work. 
+#Github is a web-based hosting service for Git repositories. 
+#Git Bash allows Windows computers to have a Unix-like environment, so you can interact with Git and other command line tools.
+# 2) What’s the difference between the terminal and the command line?
+#the command line is where you interact with the computer system by typing text commands
+#the terminal allows you to access and interact with the command line
+# 3) How does Windows PowerShell differ from Git Bash?
+#Windows powershell doesn't have the Unix-like command line experience, it's more integrated with the windows operating system
+# 4) What’s the difference between Anaconda, conda, and Python?
+#python is a programming language, conda is an open source package/environment management system
+#anaconda is a distributor of python designed for data science, machine learning, and scientific computing
+# 5) What is VS Code? 
+#vscode is an easily usable code editor that you can install
+# 6) What is a Jupyter Notebook? How is it different from Jupyter Lab?
+#jupyter notebook is like a simplified version of jupyterlab, which has more functions for complex projects
+# 7) What does ~/ mean?
+#~/ indicates a path within the home directory
+# 8) What’s the difference between an absolute path and a relative path?
+#an absolute path shows the entire path from the beginning on your computer to get to the location
+#a relative path shows only the path within the current working directory.
+# 9) Imagine you're in your "yourname" repo. Write the absolute and relative paths to "course_assignments/homework2".
+#absolute path: home/users/nandi/python_decal_fa25/course_assignments/homework2
+#relative path: python_decal_fa25/course_assignments/homework2
+# 10) What command lets you move from "course_assignments/homework2/" to "course_assignments/"?
+#cd ..
+# 11) What would rm ./ do in your current directory? (Don’t try it!)
+#it would remove the entire directory
+# 12) What do the following commands do?
+# git add: adds to the staging area
+# git commit: saves locally
+# git push: saves remotely
+
+# 13) What's the difference between "git add ." and "git add <file>"?
+#git add . stages all changes in the current directory, while git add <file> only stages the specific file you want
+# 14) What do "git status" and "git log -1" do?
+#git status shows what files are staged, which files have changes, and which files are untracked. git log -1 shows the most recent commit in the current branch
+# 15) What’s the difference between cloning a repository and pulling from it?
+#cloning a repository is what you do first, to download the entire repo to you locally. git pull updates the local repo based on changes made to the remote branch (updating the existing copy).
+# 16) What has been your most frustrating bug or error in this class so far? How did you troubleshoot or fix it?
+#because I have Windows, the initial setup was pretty frustrating for me, as I had to do lots of troubleshooting to figure out what was wrong
+# 17) What’s a question you still have? What’s something you’re confused about?
+#I don't have any questions about what we've learned so far, but I'm excited to learn about the astronomy-related Python libraries, and interpret real data.
+# 18) Tell me a fun fact! 
+#I don't know what I could possibly say about astrophysics that you don't already know, but I've been really fascinated recently by how galaxies form into a spiral structure - the way that density waves (the areas with higher density of stars and gas) behave the same way traffic jams on the highway do. So even if individual cars move in and out, the area of traffic stays the same, and this analogy applies on the scale of a galaxy. Somehow that's just really amazing to me.
+# 19) Print your favorite math expression you've learned in Python so far. 
+# (Hint: Use print() and add a comment explaining what it does.)
+print(10%3)
+#returns the remainder of 10/3.
+
